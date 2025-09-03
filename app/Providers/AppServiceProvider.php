600c1166e9a84ebfe15f98392e47f857e224c627
Towanou Luc <towanouluc@gmail.com>
2025-09-03 17:01:15 +0100

    Push du front-end

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use Dedoc\Scramble\Scramble;
 use Dedoc\Scramble\Support\Generator\OpenApi;
 use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
         Scramble::configure()->withDocumentTransformers(function (OpenApi $openApi) {
             $openApi->secure(
                 SecurityScheme::http('bearer')
